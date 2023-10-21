e18e8ee00d789372d03c79c1cc9ca82d6970fb9e
Diego <diegobqu@ucm.es>
2023-10-21 22:00:02 +0200

    Map added

--- src/components/MapSelectionPanel.js
+++ src/components/MapSelectionPanel.js
@@ -3,7 +3,7 @@ import MapDataSelection from './MapDataSelection'
 
 export default function MapSelectionPanel ({}) {
   return (
-    <section className="flex gap-2">
+    <section className="relative z-10" className="flex gap-2">
       <MapDataSelection />
       <MapDataOptions />
     </section>
